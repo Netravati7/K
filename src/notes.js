1f26c3e27cd8f5caba702fb92c99949e8304cb1e
Netravati7 <67043742+Netravati7@users.noreply.github.com>
2021-03-04 14:39:36 +0530

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,22 @@
+const notes = [
+  {
+    key: 1,
+    title: "Components",
+    content:
+      "Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML via a render() function."
+  },
+  {
+    key: 2,
+    title: "Properties",
+    content:
+      "React Props are like function arguments in JavaScript and attributes in HTML. ... Send the brand property from the Garage component to the Car component"
+  },
+  {
+    key: 3,
+    title: "States",
+    content:
+      "The state object is where you store property values that belongs to the component. When the state object changes, the component re-renders."
+  }
+];
+
+export default notes;
